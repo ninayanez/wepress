70286d6d02c7d79582348df4411a1f2a8a3a2ae8
ninayanez <nina@theblacksea.cc>
2016-10-04 17:09:09 -0700

    break up menu functionality

--- src/menu.js
+++ src/menu.js
@@ -2,36 +2,30 @@ import _ from 'underscore'
 import through from 'through2'
 
 const s = through()
-
 const menu = document.querySelector('ul.menu')
+const settings = document.querySelector('ul.settingsMenu').children
 
-_.each(menu.children,(c) => {
-  const action = c.id
-  const submenu = c.querySelector('ul')
-  
-  if (submenu) {
-    _.each(c.children, (el) => {
-      if (el.tagName !== 'UL') 
-        el.addEventListener('click', toggle, false)
-    })
-    _.each(submenu.children, (el) => {el.addEventListener('click',handler,false)})
-  } else { 
-    c.addEventListener('click',handler,false)
-  }
-})
-
-function handler (e) {
+function click (e) {
   const action = e.target.parentElement.id
   if (!action) return
   s.write(action)
 }
 
-function toggle (e) {
-  const submenu = e.target.parentElement.querySelector('ul')
-  if (submenu.tagName !== 'UL') return
-  if (submenu.style.display === 'block') 
-    submenu.style.display = 'none'
-  else submenu.style.display = 'block'
-}
+//SETTINGS MENU
+_.each(settings, (el) => {
+  const input = el.querySelector('input')
+  input.addEventListener('change', (e) => {
+    const val = e.target.value
+    const name = e.target.className
+    s.write(name+':'+val)
+  })
+})
+
+//TOOLS MENUS
+_.each(menu.children,(c) => {
+  const submenu = c.querySelector('ul')
+  if (!submenu) return
+  _.each(submenu.children, (el) => {el.addEventListener('click', click, false)})
+})
 
 export default s
